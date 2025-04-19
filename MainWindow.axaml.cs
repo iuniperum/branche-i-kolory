db5f9be1e4d3da3890d104cedb46c31a3f21ec04
iuniperum <julpietrzak147@gmail.com>
2025-04-19 19:28:18 +0200

    lets go

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,40 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace lab4;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public void load (object sender, RoutedEventArgs e) {
+        
+    }
+
+    public void rotate(object sender, RoutedEventArgs e) {
+        
+    }
+    
+    public void rotate90 (object sender, RoutedEventArgs e) {
+        
+    }
+    
+    public void rotate180 (object sender, RoutedEventArgs e) {
+        
+    }
+    
+    public void rotate270 (object sender, RoutedEventArgs e) {
+        
+    }
+    
+    public void invert (object sender, RoutedEventArgs e) {
+        
+    }
+    
+    public void green (object sender, RoutedEventArgs e) {
+        
+    }
+}
