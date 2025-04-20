16a22868d1773b97d340fc46bd7b315ae8c3311c
iuniperum <julpietrzak147@gmail.com>
2025-04-20 14:00:46 +0200

    pierwszy commit

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,17 +1,37 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using System;
+using Avalonia.Media;
 
 namespace lab4;
 
 public partial class MainWindow : Window
 {
+    private double stopien = 0;
     public MainWindow()
     {
         InitializeComponent();
     }
 
-    public void load (object sender, RoutedEventArgs e) {
-        
+    public async void load (object sender, RoutedEventArgs e) {
+        var savefiledialog = new SaveFileDialog();
+        var result = await savefiledialog.ShowAsync(this);
+        var bitmap = new Bitmap(result);
+        obraz.Source = bitmap;
+    }
+    
+    private void wybor_stopnia(object sender, RoutedEventArgs args) {
+        if (stopnie.SelectedItem is ComboBoxItem item)
+        {
+            if (item.Content is StackPanel panel)
+            {
+                if (panel.Children[0] is TextBlock text)
+                {
+                    stopien = Convert.ToDouble(text.Tag);
+                }
+            }
+        }
     }
 
     public void rotate(object sender, RoutedEventArgs e) {
