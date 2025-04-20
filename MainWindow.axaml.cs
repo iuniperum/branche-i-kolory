172a6e2b5ef3c5acd263ab6ee752a314f5a1f396
iuniperum <julpietrzak147@gmail.com>
2025-04-20 15:07:13 +0200

    zielony

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -3,12 +3,17 @@ using Avalonia.Interactivity;
 using Avalonia.Media.Imaging;
 using System;
 using Avalonia.Media;
+using AvaloniaPixelSnoop;
+using System.IO;
+using Avalonia.Skia;
+using SkiaSharp;
 
 namespace lab4;
 
 public partial class MainWindow : Window
 {
     private double stopien = 0;
+    private WriteableBitmap bitmap;
     public MainWindow()
     {
         InitializeComponent();
@@ -17,7 +22,8 @@ public partial class MainWindow : Window
     public async void load (object sender, RoutedEventArgs e) {
         var savefiledialog = new SaveFileDialog();
         var result = await savefiledialog.ShowAsync(this);
-        var bitmap = new Bitmap(result);
+        FileStream fs = File.OpenRead(result);
+        bitmap = WriteableBitmap.Decode(fs);
         obraz.Source = bitmap;
     }
     
@@ -33,18 +39,29 @@ public partial class MainWindow : Window
             }
         }
     }
-
-    public void rotate(object sender, RoutedEventArgs e) {
-        RotateTransform obrot = new RotateTransform(stopien);
-        obraz.RenderTransform = obrot;
-    }
     
+    public void green (object sender, RoutedEventArgs e) {
+        using (var snoop = new BmpPixelSnoop(bitmap)) {
+            for (int x = 0; x < bitmap.PixelSize.Width; x++)
+            {
+                for (int y = 0; y < bitmap.PixelSize.Height; y++) {
+                    var kolor = snoop.GetPixel(x, y).ToSKColor();
+                    if (kolor.Green < kolor.Red + kolor.Blue) {
+                        snoop.SetPixel(x, y, Colors.Black);
+                    }
+                }
+            }
+            obraz.Source = bitmap;
+        }
+        test.InnerLeftContent = "zielony";
+    }
     
     public void invert (object sender, RoutedEventArgs e) {
         
     }
-    
-    public void green (object sender, RoutedEventArgs e) {
-        
+
+    public void rotate(object sender, RoutedEventArgs e) {
+        RotateTransform obrot = new RotateTransform(stopien);
+        obraz.RenderTransform = obrot;
     }
 }
