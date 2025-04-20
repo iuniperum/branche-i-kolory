f179c272303f667a593da62b4decf4218d47386b
iuniperum <julpietrzak147@gmail.com>
2025-04-20 15:21:47 +0200

    ob/dwracanie

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -3,11 +3,16 @@ using Avalonia.Interactivity;
 using Avalonia.Media.Imaging;
 using System;
 using Avalonia.Media;
+using AvaloniaPixelSnoop;
+using System.IO;
+using Avalonia.Skia;
+using SkiaSharp;
 
 namespace lab4;
 
 public partial class MainWindow : Window
 {
+    private WriteableBitmap bitmap;
     private double stopien = 0;
     public MainWindow()
     {
@@ -17,7 +22,31 @@ public partial class MainWindow : Window
     public async void load (object sender, RoutedEventArgs e) {
         var savefiledialog = new SaveFileDialog();
         var result = await savefiledialog.ShowAsync(this);
-        var bitmap = new Bitmap(result);
+        FileStream fs = File.OpenRead(result);
+        bitmap = WriteableBitmap.Decode(fs);
+        obraz.Source = bitmap;
+    }
+    
+    public void green (object sender, RoutedEventArgs e) {
+        
+    }
+
+    public void upside(object sender, RoutedEventArgs e) {
+        RotateTransform obrot = new RotateTransform(180);
+        obraz.RenderTransform = obrot;
+    }
+    
+    public void invert (object sender, RoutedEventArgs e) {
+        using (var snoop = new BmpPixelSnoop(bitmap)) {
+            for (int x = 0; x < bitmap.PixelSize.Width; x++)
+            {
+                for (int y = 0; y < bitmap.PixelSize.Height; y++) {
+                    var kolor = snoop.GetPixel(x, y).ToSKColor();
+                    var nowy = new Color((byte)(255 - kolor.Red), (byte)(255 - kolor.Green), (byte)(255 - kolor.Blue), kolor.Alpha);
+                    snoop.SetPixel(x, y, nowy);
+                }
+            }
+        }
         obraz.Source = bitmap;
     }
     
@@ -39,12 +68,4 @@ public partial class MainWindow : Window
         obraz.RenderTransform = obrot;
     }
     
-    
-    public void invert (object sender, RoutedEventArgs e) {
-        
-    }
-    
-    public void green (object sender, RoutedEventArgs e) {
-        
-    }
 }
